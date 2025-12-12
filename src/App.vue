238c041e4d0055ec7831cab4671fcefba26f5f91
zhangsan-fe <3058094441@qq.com>
2025-12-12 08:19:28 +0800

    1212

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script lang="js" setup>
+    // import {fetchCat} from "./components/fetchCat.vue"
+    //经典错误
+    //{}是export多个，因此要{}包裹，fetchCat整个组件是默认导出
+    import fetchCat2 from "./components/dataSelectAndCatImg/fetchCat2.vue";
+
+    import CatImgStore from "./components/dataSelectAndCatImg/CatImgStore.vue";
+</script>
+
+<template>
+    <!-- <fetchCat></fetchCat> -->
+    <CatImgStore></CatImgStore>
+</template>
+
+<style scoped>
+</style>
